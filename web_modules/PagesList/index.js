f84b710a1c15fbe7b2b76ae754ca7fa73d9cc5cf
kevin940726 <kevin830726@gmail.com>
2016-09-02 14:14:45 +0800

    add react-virtualized

--- web_modules/PagesList/index.js
+++ web_modules/PagesList/index.js
@@ -1,4 +1,7 @@
+/* eslint-disable react/no-multi-comp, react/prop-types, react/jsx-no-bind */
 import React, { PropTypes } from "react"
+import { VirtualScroll, WindowScroller, AutoSizer } from "react-virtualized"
+import "react-virtualized/styles.css" // only needs to be imported once
 
 import PagePreview from "../PagePreview"
 import styles from "./PagesList.css"
@@ -10,11 +13,25 @@ const PagesList = ({ pages }) => {
       pages.length
       ? (
         <ul className={ styles.pageslist }>
-        {
-          pages.map((page) => (
-            <li key={ page.title }><PagePreview { ...page } /></li>
-          ))
-        }
+          <WindowScroller>
+            { ({ height, scrollTop }) => (
+              <AutoSizer disableHeight>
+                { ({ width }) => (
+                  <VirtualScroll
+                    autoHeight
+                    height={ height }
+                    rowCount={ pages.length }
+                    rowHeight={ 240 }
+                    rowRenderer={ ({ index }) => (
+                      <li key={ pages[index].title }><PagePreview { ...pages[index] } /></li>
+                    ) }
+                    width={ width }
+                    scrollTop={ scrollTop }
+                  />
+                ) }
+              </AutoSizer>
+            ) }
+          </WindowScroller>
         </ul>
       )
       : "No posts yet."
