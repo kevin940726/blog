32822937ca74c740fbbbe213969423d329079d8d
kevin940726 <kevin830726@gmail.com>
2016-09-02 14:52:03 +0800

    change footer

--- web_modules/Footer/index.js
+++ web_modules/Footer/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import { Link } from "react-router"
 
 import styles from "./index.css"
 
@@ -8,31 +7,19 @@ export default class Footer extends Component {
   render() {
     return (
       <footer className={ styles.footer }>
+        { "Powered by " }
         <a
+          target="_blank"
           href={ process.env.PHENOMIC_HOMEPAGE }
           className={ styles.link }
         >
-          { "Powered by " }
           <span className={ styles.reference }>
             {  `<${ process.env.PHENOMIC_NAME} />` }
           </span>
         </a>
 
         { " | " }
-        { "Pages: " }
-        <Link
-          className={ styles.link }
-          to="/404.html"
-        >
-          { "404" }
-        </Link>
-        { ", " }
-        <Link
-          className={ styles.link }
-          to="/loading/"
-        >
-          { "Loading" }
-        </Link>
+        { "© 2016 Kai Hao, unless otherwise noted." }
       </footer>
     )
   }
